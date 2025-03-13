8043a493044e6531e1f4a27294b4d96951578ebc
Noble Jammy Prize <chengnangong@163.com>
2025-03-13 23:46:38 +0800

    Update LICENSE

--- rpi-ups.c
+++ rpi-ups.c
@@ -1,5 +1,30 @@
+// SPDX-License-Identifier: MIT
 /*
- * 电池管理MCU驱动 - 树外内核模块
+ * Waveshare RaspberryPi UPS Driver
+ *
+ * Copyright (C) 2025 Cheng Hao
+ *
+ * This driver provides support for the Waveshare RaspberryPi UPS (Uninterruptible Power Supply)
+ * board. It includes functionality for managing the battery and USB charger, and ensures proper
+ * system shutdown in case of low battery.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include <linux/module.h>
